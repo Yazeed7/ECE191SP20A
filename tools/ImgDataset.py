a99a26dc70ec7c8c80162143e40fbbc5bab66d53
Yazeed7 <61038456+Yazeed7@users.noreply.github.com>
2020-04-30 13:07:16 -0700

    Update ImgDataset.py

--- tools/ImgDataset.py
+++ tools/ImgDataset.py
@@ -25,7 +25,7 @@ class MultiviewImgDataset(torch.utils.data.Dataset):
         parent_dir = root_dir.rsplit('/',2)[0]
         self.filepaths = []
         for i in range(len(self.classnames)):
-            all_files = sorted(glob.glob(parent_dir+'/'+self.classnames[i]+'/'+set_+'/*.png'))
+            all_files = sorted(glob.glob(parent_dir+'/'+self.classnames[i]+'/'+set_+'/*.jpg'))
             ## Select subset for different number of views
             stride = int(2/self.num_views) # 12 6 4 3 2 1
             all_files = all_files[::stride]
@@ -94,7 +94,7 @@ class SingleImgDataset(torch.utils.data.Dataset):
         parent_dir = root_dir.rsplit('/',2)[0]
         self.filepaths = []
         for i in range(len(self.classnames)):
-            all_files = sorted(glob.glob(parent_dir+'/'+self.classnames[i]+'/'+set_+'/*.png'))
+            all_files = sorted(glob.glob(parent_dir+'/'+self.classnames[i]+'/'+set_+'/*.jpg'))
             if num_models == 0:
                 # Use the whole dataset
                 self.filepaths.extend(all_files)
